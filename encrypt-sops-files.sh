424d306b16c0f95cc2909d11b800f02e7b13c23e
Rasmus John Pedersen <mail@rjp.dk>
2023-07-26 10:19:40 +0200

    feat: detect yaml,json,env based on extension

--- encrypt-sops-files.sh
+++ encrypt-sops-files.sh
@@ -4,9 +4,17 @@ EXIT_CODE=0
 
 for file in "$@"; do
 	if [[ -f "$file" ]]; then
-		if ! grep -q '"data":\s"ENC\[[A-Z0-9_]*,data:.*,type:.*\]"' "$file"; then
+		if ! grep -q '"ENC\[[A-Z0-9_]*,data:.*,type:.*\]"' "$file"; then
 			echo "File $file is not encrypted, encrypting..."
-			sops --encrypt --in-place "$file"
+			if [[ "$file" =~ \.yaml ]]; then
+				sops --encrypt --in-place --input-type=yaml "$file"
+			elif [[ "$file" =~ \.json ]]; then
+				sops --encrypt --in-place --input-type=json "$file"
+			elif [[ "$file" =~ \.env ]]; then
+				sops --encrypt --in-place --input-type=dotenv "$file"
+			else
+				sops --encrypt --in-place "$file"
+			fi
 			EXIT_CODE=1
 		fi
 	fi
